974572a96554dca77a64a9d25d64c82b689f59dd
Shahrukh <Shahrukhktk760@gmail.com>
2020-12-15 16:17:59 +0500

    init 1. sign up done 2. login done 3. congrats done 4. reset password done 5. profile done 6. edit profile done 7. add vehicle --------------------------- 2nd order screens 1. home screen 2. chatlist screen done 3. conversation screen done

--- lib/All_Screens/main_screens/home_screen.dart
+++ lib/All_Screens/main_screens/home_screen.dart
@@ -46,14 +46,55 @@ class _HomeScreenState extends State<HomeScreen> {
                             padding: const EdgeInsets.only(top: 8.0),
                             child: Text('quelle voiture a besoin de notre aide?', style: TextStyle(fontSize: 13, fontWeight: FontWeight.normal, color: Colors.grey, fontFamily: "Playfair_Display"),),
                           ),
-
                         ],
                       ),
+                    ),
+                  ],
+                ),
+              ),
+
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15, left: 5.0,),
+                      child: Image(image: AssetImage('images/car1.png'),),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15, left: 5.0,),
+                      child: Image(image: AssetImage('images/car2.png'),),
                     ),
 
                   ],
                 ),
               ),
+
+              Padding(
+                padding: const EdgeInsets.only(left: 8, top: 15.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text('Mini Cooper', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.black, fontFamily: "Playfair_Display"),),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10.0),
+                          child: Text('2018', style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal, color: Colors.grey, fontFamily: "Playfair_Display"),),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: Text('Moteur turbo diesel de 2,0 L', style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal, color: Colors.grey, fontFamily: "Playfair_Display"),),
+                    ),
+                  ],
+                ),
+              ),
+
 
               
             ],
